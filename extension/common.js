12faf8a0e12e92dda82446bf4599cd07f621bff6
Anderson Juhasc <anderson.juhasc@gmail.com>
2024-04-27 15:30:14 -0300

    improved encryption #7

--- extension/common.js
+++ extension/common.js
@@ -1,10 +1,31 @@
 import CryptoJS from 'crypto-js'
 
-export function encrypt(data, password) {
-  return CryptoJS.AES.encrypt(JSON.stringify(data), password).toString()
+function deriveKey(password, salt) {
+  const iterations = 10000
+  const keyLength = 256
+  return CryptoJS.PBKDF2(password, salt, { keySize: keyLength / 32, iterations: iterations })
 }
 
-export function decrypt(ciphertext, password) {
-  const bytes = CryptoJS.AES.decrypt(ciphertext, password)
-  return JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
+export function encrypt(data, password) {
+  const salt = CryptoJS.lib.WordArray.random(128 / 8)
+  const derivedKey = deriveKey(password, salt)
+  const iv = CryptoJS.lib.WordArray.random(128 / 8)
+  const encrypted = CryptoJS.AES.encrypt(
+    JSON.stringify(data),
+    derivedKey,
+    { iv: iv }
+  )
+
+  // Convert the salt, IV, and encrypted data to a single string
+  return salt.toString() + iv.toString() + encrypted.toString()
+}
+
+export function decrypt(encryptedData, password) {
+  const salt = CryptoJS.enc.Hex.parse(encryptedData.substring(0, 32))
+  const iv = CryptoJS.enc.Hex.parse(encryptedData.substring(32, 64))
+  const encrypted = encryptedData.substring(64)
+  const derivedKey = deriveKey(password, salt)
+  const decrypted = CryptoJS.AES.decrypt(encrypted, derivedKey, { iv: iv })
+
+  return JSON.parse(decrypted.toString(CryptoJS.enc.Utf8))
 }
