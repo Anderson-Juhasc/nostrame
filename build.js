d57712829836ea8e76a74207344b2b3d5b73f310
Anderson Juhasc <anderson.juhasc@gmail.com>
2024-05-17 11:55:10 -0300

    updated build script to watch scss files

--- build.js
+++ build.js
@@ -1,24 +1,68 @@
 #!/usr/bin/env node
 
-const esbuild = require('esbuild')
+import * as esbuild from 'esbuild'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as sass from 'sass'
+import * as chokidar from 'chokidar'
 
 const prod = process.argv.indexOf('prod') !== -1
 
-esbuild
-  .build({
-    bundle: true,
-    entryPoints: {
-      'popup.build': './extension/popup.jsx',
-      //'prompt.build': './extension/prompt.jsx',
-      'options.build': './extension/options.jsx',
-      'background.build': './extension/background.js',
-      //'content-script.build': './extension/content-script.js'
-    },
-    outdir: './extension',
-    sourcemap: prod ? false : 'inline',
-    define: {
-      window: 'self',
-      global: 'self'
-    }
+function buildSass() {
+  const sassEntryFile = './extension/assets/css/style.scss';  // Your main Sass file
+  const cssOutputFile = './extension/assets/css/style.build.css'; // Output CSS file
+
+  const result = sass.renderSync({
+    file: sassEntryFile,
+    outFile: cssOutputFile,
+    sourceMap: true,
+    outputStyle: 'compressed',
   })
-  .then(() => console.log('build success.'))
+
+  fs.mkdirSync(path.dirname(cssOutputFile), { recursive: true })
+  fs.writeFileSync(cssOutputFile, result.css)
+  if (result.map) {
+    fs.writeFileSync(`${cssOutputFile}.map`, result.map)
+  }
+
+  console.log('Sass compiled successfully')
+}
+
+// Build Sass initially
+buildSass()
+
+// Watch for changes in Sass files
+chokidar.watch('./extension/assets/css/*.scss').on('change', (event) => {
+  console.log(`File ${event} has been changed`)
+  buildSass()
+});
+
+function buildEsbuild() {
+  esbuild
+    .build({
+      bundle: true,
+      entryPoints: {
+        'popup.build': './extension/popup.jsx',
+        //'prompt.build': './extension/prompt.jsx',
+        'options.build': './extension/options.jsx',
+        'background.build': './extension/background.js',
+        //'content-script.build': './extension/content-script.js'
+      },
+      outdir: './extension',
+      sourcemap: prod ? false : 'inline',
+      define: {
+        window: 'self',
+        global: 'self'
+      }
+    })
+    .then(() => console.log('Build success.'))
+}
+
+// Build esbuild and watch for changes
+buildEsbuild()
+
+// Watch for changes in Sass files
+chokidar.watch('./extension/**/*.{jsx,jsx}').on('change', (event) => {
+  console.log(`File ${event} has been changed`)
+  buildEsbuild()
+});
