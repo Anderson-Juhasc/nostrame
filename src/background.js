cb47557ca1353790fbf7840e2163e3028aadd611
Anderson Juhasc <anderson.juhasc@gmail.com>
2024-10-02 18:13:00 -0300

    added more relays

--- src/background.js
+++ src/background.js
@@ -2,5 +2,12 @@ import browser from 'webextension-polyfill'
 
 // Function to handle when the extension is installed or updated
 browser.runtime.onInstalled.addListener(async function() {
-  await browser.storage.local.set({ "relays": ["wss://relay.damus.io", "wss://nos.lol"] })
+  await browser.storage.local.set({ 
+    "relays": [
+      "wss://relay.damus.io",
+      "wss://nos.lol",
+      "wss://nostr.bitcoiner.social",
+      "wss://offchain.pub",
+    ] 
+  })
 })
